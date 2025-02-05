37365f4bd46e67e0b4c18e7f5b021700c43b3fcc
Afonso Goncalves Mota <afogonca@c2r2s2.42porto.com>
2025-02-05 08:44:36 +0000

    res

--- srcs/ft_redirects/ft_redirects.c
+++ srcs/ft_redirects/ft_redirects.c
@@ -68,13 +68,78 @@ t_token *ft_put_cmd_first(t_token *tokens)
 	return (head);
 }
 
+t_token	*ft_take_ro_out(t_token *head, t_token *tokens)
+{
+	t_token	*tmp;
+	t_token	*tmp2;
+
+	tmp = head;
+	while (tmp && tmp->type != PIPE)
+	{
+		tmp2 = tmp->next;
+		if (tmp->next && (tmp->next->type == D_REDIRECT_OUT
+		|| tmp->next->type == REDIRECT_OUT) && tmp->next->next 
+			&& tmp->next != tokens && tmp->next->next->next)
+		{
+			tmp->next = tmp->next->next->next;
+			ft_free_token(tmp2);
+		}
+		else if (tmp->next && (tmp->next->type == D_REDIRECT_OUT
+		|| tmp->next->type == REDIRECT_OUT) && tmp->next->next 
+			&& tmp->next != tokens)
+		{
+			tmp->next = NULL;
+			ft_free_token(tmp2);
+		}
+		else
+			tmp = tmp->next;
+	}
+	tokens = head;
+	return (NULL);
+}
+
+void	ft_redir_short_out(t_token *tokens)
+{
+	t_token	*head;
+	t_token	*tmp;
+	int		i;
+
+	i = 0;
+	while (tokens)
+	{
+		tmp = NULL;
+		head = tokens;
+		while (tokens && tokens->type != PIPE)
+		{
+			if ((tokens->type == D_REDIRECT_OUT || tokens->type == REDIRECT_OUT))
+			{
+				i++;
+				if (access(tokens ->next->content, W_OK) == -1)
+				{
+					tmp = ft_take_ro_out(tokens, tmp);
+					break ;
+				}
+				if (tokens->type == D_REDIRECT_OUT || tokens->type == REDIRECT_OUT)
+					tmp = tokens;
+			}
+			tokens = tokens->next;
+		}
+		if (tmp)
+		{
+			tokens = head;
+			tokens = ft_take_ros_out(tokens, tmp);
+			tokens = ft_take_rod_out(tokens, tmp);
+		}
+	}
+}
+
 int	ft_redirects(t_token *tokens, t_data **data)
 {
-	/*tokens = ft_put_cmd_first(tokens);*/
+	tokens = ft_put_cmd_first(tokens);
 	ft_redir_short_out_single(tokens);
 	ft_redir_short_out_double(tokens);
+	ft_redir_short_out(tokens);
 	(*data)->tokens_start = tokens;
 	(*data)->tokens = tokens;
-	printf("after\n");
 	return (0);
 }
